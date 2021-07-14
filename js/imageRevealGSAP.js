ed4a5435430c6fb787e32cbea209321cd87f0a9f
jacekmatuszyk <87014191+jacekmatuszyk@users.noreply.github.com>
2021-07-14 13:39:39 +0200

    Image reveal now from bottom to top

--- js/imageRevealGSAP.js
+++ js/imageRevealGSAP.js
@@ -13,11 +13,11 @@ revealContainers.forEach((container) => {
 
   tl.set(container, { autoAlpha: 1 });
   tl.from(container, 1.5, {
-    xPercent: -100,
+    yPercent: 100,
     ease: Power2.out
   });
   tl.from(image, 1.5, {
-    xPercent: 100,
+    yPercent: -100,
     scale: 1.3,
     delay: -1.5,
     ease: Power2.out
