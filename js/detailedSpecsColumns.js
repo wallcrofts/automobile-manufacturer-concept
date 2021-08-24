24d8ab6accc01fae2ffb7b3c4ff46115e859862b
jacekmatuszyk <87014191+jacekmatuszyk@users.noreply.github.com>
2021-08-24 12:57:57 +0200

    removed unused scripts

--- js/detailedSpecsColumns.js
+++ js/detailedSpecsColumns.js
@@ -1,22 +1,13 @@
 $("document").ready(function(){
     $(".tab-slider--body").hide();
     $(".tab-slider--body:first").show();
-  });
+});
   
-  
-  $(".specs-options-nav li").click(function() {
-
+$(".specs-options-nav li").click(function() {
     $(".tab-slider--body").hide();
     var activeTab = $(this).attr("rel");
     $("#"+activeTab).fadeIn();
-      if($(this).attr("rel") == "tab2"){
-          $('#specs-options-nav-items').addClass('slide');
-      }else{
-          $('#specs-options-nav-items').removeClass('slide');
-      }
-
     $(".specs-options-nav li").removeClass("active");
-
     $(this).addClass("active");
-  });
+});
   
